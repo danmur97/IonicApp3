47abbb662fa61d2371ae69793c059bc94ecfc629
danmur97 <danmur97@outlook.com>
2018-09-11 11:11:48 -0500

    init

---
+++ src/models/producto.ts
@@ -0,0 +1,10 @@
+export class Producto
+{
+    id?: string;
+    bestSeller?: boolean;//es opcional por eso el ?
+    nombre?: string;
+    descripcion?: string;
+    precio?: string;
+    imagen?: string;
+    genero?: string;
+}
